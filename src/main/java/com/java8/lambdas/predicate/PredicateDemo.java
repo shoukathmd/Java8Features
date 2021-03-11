7365b0a6ab1ddacaf500b74f041cbf677d077759
Mohammed Shoukath Ali <shoukathmd@gmail.com>
2021-03-11 12:46:08 +0530

    Predicate : Plain, with and/or/negate

--- src/main/java/com/java8/lambdas/predicate/PredicateDemo.java
+++ src/main/java/com/java8/lambdas/predicate/PredicateDemo.java
@@ -9,14 +9,29 @@ import java.util.function.Predicate;
 public class PredicateDemo {
 
     public static boolean isPersonEligibleForVote(Person person, Predicate<Person> predicate) {
-        return predicate.test(person);
+        return predicate.negate().test(person);
     }
 
     public static void main(String[] args) {
         Person p1 = new Person("Alex", 18, "USA");
 
+        Predicate<Person> greaterThan = person -> person.getAge() > 20;
+
+        Predicate<Person> lessThan = person -> person.getAge() < 60;
+
         boolean value = isPersonEligibleForVote(p1, (person -> person.getAge() > 20));
         System.out.println("Person's elgibility : " + value);
+
+        //Predicate and then
+
+        boolean value1 = isPersonEligibleForVote(p1, greaterThan.and(lessThan));
+        System.out.println("Person's elgibility and  less than : " + value1);
+
+        Predicate<Person> isFromUSA = person -> "USA".equals(person.getCountry());
+
+        boolean value3 = isPersonEligibleForVote(p1, greaterThan.or(isFromUSA));
+        System.out.println("Person's elgibility and  less than or from USA : " + value3);
+
     }
 
 }
